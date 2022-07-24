eeac358a98bb9ed9e1a5ff94126fc065623952a2
IcePlayer <nathanchevalet@orange.fr>
2022-07-24 11:56:13 +0200

    add login & signup roads

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+const routes = [{
+  path: '/',
+  name: 'home',
+  component: HomeView
+}, {
+  path: '/about',
+  name: 'about',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+}, {
+  path: '/auth',
+  component: () => import(/* webpackChunkName: "auth" */ '../views/AuthView.vue'),
+  children: [{
+    path: 'login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/auth/LoginView.vue')
+  }, {
+    path: 'signup',
+    name: 'signup',
+    component: () => import(/* webpackChunkName: "signup" */ '../views/auth/SignupView.vue')
+  }]
+}]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
