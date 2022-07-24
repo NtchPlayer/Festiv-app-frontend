58e8995e646018a5ce1f0de11fbca18e34d5f8ba
IcePlayer <nathanchevalet@orange.fr>
2022-07-24 12:01:04 +0200

    import faCircleExclamation icon

--- src/fontawesome/index.js
+++ src/fontawesome/index.js
@@ -5,9 +5,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faUserSecret)
+library.add(faCircleExclamation)
 
 export default FontAwesomeIcon
