05ccd96875e50c626f5dd1de83d1e1367ebd50f2
IcePlayer <nathanchevalet@orange.fr>
2022-07-24 16:05:35 +0200

    fix baseURL

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://localhost:3000/'
 })
 
 export {
