abaca399857dc92a67c00c1dba40421300aebc29
IcePlayer <nathanchevalet@orange.fr>
2022-07-25 11:53:30 +0200

    revamp file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
+  <AsideMenu v-if="isAuth" />
   <router-view/>
 </template>
+
+<script>
+import AsideMenu from '@/components/AsideMenu'
+
+export default {
+  name: 'App',
+  components: {
+    AsideMenu
+  },
+  computed: {
+    isAuth () {
+      return this.$store.state.auth.status.loggedIn
+    }
+  }
+}
+
+</script>
 
 <style src="./style/reset.css" />
 <style lang="scss" src="./style/main.scss" />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="main-container">
+    <Header title="Accueil"/>
+    <section>
+
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
