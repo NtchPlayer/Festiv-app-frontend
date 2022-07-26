12b11b008516de6f586324d45b9598b2648b1c2c
IcePlayer <nathanchevalet@orange.fr>
2022-07-26 10:55:53 +0200

    add style

--- src/components/ProfilePicture.vue
+++ src/components/ProfilePicture.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container-profile-picture">
-    <router-link :to="{ name: 'profile', params: { user: name } }">
+<!--    <router-link :to="{ name: 'profile', params: { user: name } }">-->
+    <a href="/">
       <img
         class="profile-picture"
         alt="Photo de profile de l'utilisateur"
-        :src="src"
+        :src="require('@/assets/default-profile-picture.jpg')"
       >
-    </router-link>
+    </a>
+<!--    </router-link>-->
   </div>
 </template>
 
@@ -19,3 +21,26 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.container-profile-picture{
+  width: 45px;
+  min-width: 45px;
+  max-width: 45px;
+  height: 45px;
+  min-height: 45px;
+  max-height: 45px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+
+.profile-picture{
+  display: block;
+  object-fit: contain;
+  width: 100%;
+  transition-duration: var(--transition-duration);
+  &:hover{
+    filter: brightness(.8);
+  }
+}
+</style>
