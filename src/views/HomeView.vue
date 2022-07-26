eec2c37a0fe62c953712255c3706edb869a4f292
IcePlayer <nathanchevalet@orange.fr>
2022-07-26 10:53:45 +0200

    revamp file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,62 @@
 <template>
   <main class="main-container">
-    <Header title="Accueil"/>
-    <section>
-
+    <Header
+      title="Accueil"
+      :button="{
+        label: 'Nouveau Post',
+        icon: 'fa-plus'
+      }"
+      @emitClick="newPost"
+    />
+    <section v-if="newPostModal" class="container-modal" @click.self="newPostModal = false">
+      <div class="modale">
+        <CreatePublication @emitClose="newPostModal = false" />
+      </div>
     </section>
+    <section>
+      <CreatePublication />
+    </section>
+    <section>
+      <ItemPublication
+        v-for="publication of publications"
+        :key="publication.id"
+        :publication="publication"
+      />
+    </section>
+    <NavMenu />
   </main>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import NavMenu from '@/components/NavMenu'
+import CreatePublication from '@/components/CreatePublication'
+import ItemPublication from '@/components/ItemPublication'
 
 export default {
   name: 'HomeView',
   components: {
+    ItemPublication,
+    CreatePublication,
+    NavMenu,
     Header
+  },
+  data () {
+    return {
+      newPostModal: false,
+      publications: null
+    }
+  },
+  mounted () {
+    this.axios.get('publications')
+      .then((e) => {
+        this.publications = e.data
+      })
+  },
+  methods: {
+    newPost () {
+      this.newPostModal = true
+    }
   }
 }
 </script>
