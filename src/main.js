3f009d0dbd7d3c57d0d15ccede7adfc2fd7f7007
IcePlayer <nathanchevalet@orange.fr>
2022-07-26 12:00:14 +0200

    add filter for time

--- src/main.js
+++ src/main.js
@@ -6,9 +6,23 @@ import store from './store'
 import FontAwesomeIcon from './fontawesome'
 import { axiosInstance, VueAxios } from '@/axios'
 
-createApp(App)
+const app = createApp(App)
   .use(store)
   .use(router)
   .use(VueAxios, axiosInstance)
   .component('font-awesome-icon', FontAwesomeIcon)
-  .mount('#app')
+
+app.config.globalProperties.$filters = {
+  timeFilter (value) {
+    const publicationTime = new Date(value)
+    const currentDate = new Date()
+    const monthsList = ['janv.', 'fév.', 'mars', 'avr.', 'mai', 'juin', 'juil.', 'aout', 'sept.', 'oct.', 'nov.', 'dec.']
+
+    const month = monthsList[publicationTime.getMonth()]
+    const years = currentDate.getFullYear() !== publicationTime.getFullYear() ? publicationTime.getFullYear() : ''
+
+    return `${publicationTime.getDay()} ${month} ${years}`
+  }
+}
+
+router.isReady().then(() => app.mount('#app'))
