65ee7112b563248bfede117b1f980c8f6674477d
IcePlayer <nathanchevalet@orange.fr>
2022-07-26 10:55:14 +0200

    comment all file

--- src/components/UserInfos.vue
+++ src/components/UserInfos.vue
@@ -1,24 +1,24 @@
-<template>
-  <div class="user-infos">
-    <ProfilePicture name="IcePlayer" />
-    <div>
-      <span class="user-infos-username" v-text="$store.state.auth.user.username" />
-      <span class="user-infos-email" v-text="$store.state.auth.user.email" />
-    </div>
-  </div>
-</template>
+<!--<template>-->
+<!--  <div class="user-infos">-->
+<!--    <ProfilePicture name="IcePlayer" />-->
+<!--    <div>-->
+<!--      <span class="user-infos-username" v-text="$store.state.auth.user.username" />-->
+<!--      <span class="user-infos-email" v-text="$store.state.auth.user.email" />-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
 
-<script>
-import ProfilePicture from '@/components/ProfilePicture'
+<!--<script>-->
+<!--import ProfilePicture from '@/components/ProfilePicture'-->
 
-export default {
-  name: 'UserInfos',
-  components: {
-    ProfilePicture
-  }
-}
-</script>
+<!--export default {-->
+<!--  name: 'UserInfos',-->
+<!--  components: {-->
+<!--    ProfilePicture-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
 
-<style lang="scss">
+<!--<style lang="scss">-->
 
-</style>
+<!--</style>-->
