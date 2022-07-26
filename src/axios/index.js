fea2ba26662a1be67c1982107bce9b2c2a0ee586
IcePlayer <nathanchevalet@orange.fr>
2022-07-26 10:54:02 +0200

    add authorisation header gestion

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import authHeader from '../services/auth-header.service'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/'
+  baseURL: 'http://localhost:3000/',
+  headers: authHeader()
 })
 
 export {
