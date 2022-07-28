3c2d37b0567e9d01a42756eaf3b9d9f5a496b58a
IcePlayer <nathanchevalet@orange.fr>
2022-07-28 15:55:44 +0200

    add option for item & delete method

--- src/components/ItemPublication.vue
+++ src/components/ItemPublication.vue
@@ -3,19 +3,33 @@
     <ProfilePicture :name="publication.user.username" />
     <div class="publication-content">
       <header class="publication-header">
-        <h1
-          class="publication-header-username"
-          v-text="publication.user.username"
-        />
-        •
-        <time
-          class="publication-header-time"
-          v-text="$filters.timeFilter(publication.createdAt)"
-        />
+        <div class="publication-header-meta">
+          <h1
+            class="publication-header-username"
+            v-text="publication.user.username"
+          />
+          •
+          <time
+            class="publication-header-time"
+            v-text="$filters.timeFilter(publication.createdAt)"
+          />
+        </div>
+        <div class="publication-options-container">
+          <button class="button-round" type="button" @click.prevent="optionsMenu = true">
+            <font-awesome-icon icon="fa-solid fa-ellipsis" />
+          </button>
+          <div v-show="optionsMenu" class="publication-options-button-container">
+            <button type="button" class="red publication-options-button" @click.prevent="deletePost">
+              <font-awesome-icon class="publication-options-button-icon" icon="fa-regular fa-trash-can" />
+              <span class="publication-options-button-label">Supprimer la publication</span>
+            </button>
+          </div>
+          <div v-show="optionsMenu" class="publication-options-mask" @click.prevent="optionsMenu = false"/>
+        </div>
       </header>
-      <main class="publication-body" v-html="publication.content" />
+      <main v-html="publication.content" />
       <footer class="publication-footer">
-        <button class="button-round button-heart">
+        <button type="button" class="button-round button-heart">
           <font-awesome-icon icon="fa-regular fa-heart" />
         </button>
       </footer>
@@ -33,6 +47,19 @@ export default {
   },
   props: {
     publication: { type: Object, required: true }
+  },
+  data () {
+    return {
+      optionsMenu: false
+    }
+  },
+  methods: {
+    deletePost () {
+      this.axios.delete(`publications/${this.publication.id}`)
+        .then(() => {
+          this.$emit('fetchPublications')
+        })
+    }
   }
 }
 </script>
