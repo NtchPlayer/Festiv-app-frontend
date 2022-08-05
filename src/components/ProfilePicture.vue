1619efa58ee97a25ff5f826bd7d38d8d081b2a6c
IcePlayer <nathanchevalet@orange.fr>
2022-08-05 14:35:02 +0200

    revamp file

--- src/components/ProfilePicture.vue
+++ src/components/ProfilePicture.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="container-profile-picture" :class="{ 'profile-picture-large': largeIcon }">
-    <router-link :to="{ name: 'profile', params: { name }}">
+  <div
+    class="container-profile-picture"
+    :class="{
+      'profile-picture-large': largeIcon,
+      'container-profile-picture-hover': !isEditable
+    }"
+  >
+    <component
+      :is="isEditable ? 'div' : 'router-link'"
+      :to="isEditable ? null : { name: 'profile', params: { name }}"
+    >
       <figure>
         <img
           class="profile-picture"
           alt="Photo de profile de l'utilisateur"
-          :src="require('@/assets/default-profile-picture.jpg')"
+          :src="src"
         >
+        <div v-if="isEditable">
+        <label type="button" class="button-round button-black" for="profile-image">
+          <span class="button-round-icon">
+            <font-awesome-icon icon="fa-solid fa-image" />
+          </span>
+        </label>
+        <input
+          id="profile-image"
+          class="input-file"
+          type="file"
+          ref="input-profile"
+          @change="__emitFile"
+        >
+        </div>
       </figure>
-    </router-link>
+    </component>
   </div>
 </template>
 
@@ -16,9 +39,15 @@
 export default {
   name: 'ProfilePicture',
   props: {
-    src: { type: String, default: '@/assets/default-profile-picture.jpg' },
+    src: { type: String, default: require('@/assets/default-profile-picture.jpg') },
     name: { type: String, required: true },
-    largeIcon: { type: Boolean, default: false }
+    largeIcon: { type: Boolean, default: false },
+    isEditable: { type: Boolean, default: false }
+  },
+  methods: {
+    __emitFile () {
+      this.$emit('emitFile', this.$refs['input-profile'].files[0])
+    }
   }
 }
 </script>
@@ -33,13 +62,29 @@ export default {
   max-height: 45px;
   border-radius: 50%;
   overflow: hidden;
+  position: relative;
+  transition-duration: var(--transition-duration);
+  div,
+  a,
+  figure{
+    height: 100%;
+  }
+  &-hover:hover{
+    filter: brightness(.8);
+  }
+  .button-round{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 }
 
 .profile-picture{
   display: block;
-  object-fit: contain;
+  object-fit: cover;
   width: 100%;
-  transition-duration: var(--transition-duration);
+  height: 100%;
   &-large{
     width: 100px;
     min-width: 100px;
@@ -47,9 +92,6 @@ export default {
     height: 100px;
     min-height: 100px;
     max-height: 100px;
-  }
-  &:hover{
-    filter: brightness(.8);
   }
 }
 </style>
