69a90acd8b6347e1bb2dccc75ad98b347e3dee51
IcePlayer <nathanchevalet@orange.fr>
2022-08-11 15:28:17 +0200

    add logic for confirm & old password check

--- src/components/fields/FieldPassword.vue
+++ src/components/fields/FieldPassword.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="container-input">
-    <label class="label" for="password">
-      Mot de passe
-    </label>
+    <label class="label" for="password" v-text="label" />
     <input
       id="password"
       ref="input"
       v-model="password"
       class="input"
-      :class="{'input-error': error}"
       type="password"
       placeholder="Votre mot de passe"
       :disabled="disabled"
@@ -16,7 +13,18 @@
       :minlength="errorChecker === false ? false : 8"
       :autocomplete="autocomplete"
     >
-    <div v-if="errorChecker" v-show="error" class="container-warning-field">
+    <template v-if="errorChecker">
+      <p
+        v-if="checkType !== 'new'"
+        v-show="!isValid && this.modelValue !== ''"
+        class="warning container-warning-field"
+      >
+        <span aria-hidden="true" class="color-red">
+          <font-awesome-icon icon="fa-solid fa-circle-exclamation" />
+        </span>
+        {{ warningMessage }}
+      </p>
+      <div v-else v-show="!isValid && modelValue !== ''" class="container-warning-field">
       <span class="color-red">
         <font-awesome-icon icon="fa-solid fa-circle-exclamation" />
       </span>
@@ -28,27 +36,28 @@
           v-text="'aucun espace'"
         />
         <li
-          v-show="passwordLengthMet"
+          v-show="!passwordLengthMet"
           class="item-list-warning"
           v-text="'avoir au moins 8 caractère'"
         />
         <li
-          v-show="passwordHasCap"
+          v-show="!passwordHasCap"
           class="item-list-warning"
           v-text="'avoir au moins une majuscule'"
         />
         <li
-          v-show="passwordHasNumber"
+          v-show="!passwordHasNumber"
           class="item-list-warning"
           v-text="'avoir au moins un nombre'"
         />
         <li
-          v-show="passwordHasSpecialCharacter"
+          v-show="!passwordHasSpecialCharacter"
           class="item-list-warning"
           v-text="'avoir un caractère spécial'"
         />
       </ul>
     </div>
+    </template>
   </div>
 </template>
 
@@ -61,34 +70,49 @@ export default {
     disabled: { type: Boolean, default: false },
     focus: { type: Boolean, default: false },
     modelValue: { type: String, default: '' },
-    errorChecker: { type: Boolean, default: false }
+    warningMessage: { type: String, default: 'Les deux mots de passe ne correspondent pas.' },
+    errorChecker: { type: Boolean, default: false },
+    checkType: { type: String, default: 'new' },
+    // checkOld: { type: Boolean, default: false },
+    // confirmPassword: { type: Boolean, default: false },
+    passwordToCheck: { type: String, default: '' },
+    label: { type: String, default: 'Mot de passe' }
   },
   computed: {
     passwordLengthMet () {
-      return (this.password.trim().length < 8)
+      return this.password.trim().length > 8
     },
     passwordHasCap () {
-      return !(/[A-Z]/.test(this.password.toString()))
+      return /[A-Z]/.test(this.password.toString())
     },
     passwordHasNumber () {
-      return !(/\d/.test(this.password.toString()))
+      return /\d/.test(this.password.toString())
     },
     passwordHasSpecialCharacter () {
-      return !(/[&#@!?§%€$="+_:/,;'.-]/.test(this.password.toString()))
+      return /[&#@!?§%€$="+_:/,;'.-]/.test(this.password.toString())
     },
     passwordHasSpace () {
-      return (/\s+/.test(this.password.toString()))
+      return /\s+/.test(this.password.toString())
     },
-    error () {
-      return (this.passwordLengthMet || this.passwordHasCap || this.passwordHasNumber || this.passwordHasSpace || this.passwordHasSpecialCharacter) && this.password !== ''
+    passwordIsSame () {
+      return this.modelValue === this.passwordToCheck && this.passwordToCheck !== ''
     },
-    passwordIsValid () {
-      return !this.error && this.password !== ''
+    isValid () {
+      switch (this.checkType) {
+        case 'old':
+          return !this.passwordIsSame
+        case 'confirm':
+          return this.passwordIsSame
+        case 'new':
+          return this.passwordLengthMet && this.passwordHasCap && this.passwordHasNumber && !this.passwordHasSpace && this.passwordHasSpecialCharacter
+        default:
+          return true
+      }
     }
   },
   watch: {
-    passwordIsValid () {
-      this.$emit('passwordIsValid', this.passwordIsValid)
+    isValid () {
+      this.$emit('isValid', this.isValid)
     }
   },
   mounted () {
