f682501cf12a8eb0f6817d598c3cf405b05a685c
IcePlayer <nathanchevalet@orange.fr>
2022-08-12 00:26:25 +0200

    block check user on logout

--- src/router/beforeEach.js
+++ src/router/beforeEach.js
@@ -1,7 +1,7 @@
 import store from '@/store'
 
 export default async function beforeEach ({ to, from, next }) {
-  if (to.meta.requireAuth) {
+  if (to.meta.requireAuth && to.name !== 'login') {
     await store.dispatch('auth/checkUserSession')
   }
   if (!store.state.auth.status.loggedIn && to.meta.requireAuth) {
